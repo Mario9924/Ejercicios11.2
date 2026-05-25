a1a1c85b46aff139951ad30e2d23a6930485c159
Mario Gutierrez <169930559+Mario9924@users.noreply.github.com>
2026-05-26 00:20:58 +0200

    Se realiza el apartado F. Hay un problema, el campo procedencia incluye valores NULOS lo que hace necesario el comprobar que el ResultSet correspondiente NO devuelva nulos y con ello evitar una excepción

--- src/ejercicios112/Ejercicio1.java
+++ src/ejercicios112/Ejercicio1.java
@@ -9,20 +9,20 @@ import java.util.Scanner;
 
 /**
  * Mediante el uso de la BD nba vamos a mostrar al usuario una serie de opciones
- * a. Quién es el jugador o jugadores más alto de la NBA. 
- * b. Quién es el jugador o jugadores más bajo de la NBA. 
- * c. Quienes son los jugadores de los Lakers.
- * d. Quién es el jugador con más puntos por partido (obvio que es un error). 
- * e. Un listado con los nombres cuyo apellido empieza por J. 
- * f. Número de jugadores españoles y listado de sus nombres y equipos. 
- * g. El número de puntos más alto de cualquier equipo en un partido (ya sea local o visitante).
+ * a. Quién es el jugador o jugadores más alto de la NBA. b. Quién es el jugador
+ * o jugadores más bajo de la NBA. c. Quienes son los jugadores de los Lakers.
+ * d. Quién es el jugador con más puntos por partido (obvio que es un error). e.
+ * Un listado con los nombres cuyo apellido empieza por J. f. Número de
+ * jugadores españoles y listado de sus nombres y equipos. g. El número de
+ * puntos más alto de cualquier equipo en un partido (ya sea local o visitante).
  * h. El único partido donde tanto locales como visitantes hicieron el número de
  * puntos más alto (el obtenido antes). Obvio que esto es un error de la BD, un
  * partido de baloncesto no puede terminar empatado.
- * 
- * 
+ *
+ *
  * @author Mario Gutiérrez
- * @see https://classroom.google.com/u/1/c/ODA3NDY5OTY5MTcz/a/ODY0OTY3MTEwMzc0/details
+ * @see
+ * https://classroom.google.com/u/1/c/ODA3NDY5OTY5MTcz/a/ODY0OTY3MTEwMzc0/details
  * @version 1.0
  */
 public class Ejercicio1 {
@@ -40,23 +40,20 @@ public class Ejercicio1 {
         String user = "root";
         String pass = "";
         try (
-                Connection conn = DriverManager.getConnection(url, user, pass);
-                Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
-                ResultSet rs = stmt.executeQuery("Select * from jugadores"); // USAR PARA CONSULTAS
-                
-            ){
-            
+                Connection conn = DriverManager.getConnection(url, user, pass); Statement stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE); ResultSet rs = stmt.executeQuery("Select * from jugadores"); // USAR PARA CONSULTAS
+                ) {
+
             //se carga la clase del Driver
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
+
             /*
                 Para los apartados A y B calculamos la altura media y luego mostramos la información
                  que corresponda en cada caso.
             
                 La altura está almacenada en el formato String "6-9", lo "convertimos en double" tras 
                     reemplazar el caracter '-' por '.' de modo que quedará cómo "6.9"
-            */
-            /*
+             */
+ /*
             rs.beforeFirst();
             
             while(rs.next()){
@@ -97,7 +94,7 @@ public class Ejercicio1 {
                 }
             }
             System.out.println("");
-            */
+            
             
             // E - Un listado con los nombres cuyo apellido empieza por J.
             rs.beforeFirst();
@@ -115,13 +112,31 @@ public class Ejercicio1 {
                     System.err.println("NOMBRE SIN ESPACIO EN BLANCO: " + rs.getString("Nombre"));
                 }
             }
-            
-        }  catch(SQLException sqlex){
+             */
+            // F - Número de jugadores españoles y listado de sus nombres y equipos.
+            System.out.println("");
+            rs.beforeFirst();
+            System.out.println("Jugadores españoles: ");
+            while (rs.next()) {
+                /*
+                    Hay campos cuya campo "procdencia" es nula por ello hay que comprobarlo primero
+                    En caso contrario tendremos excepciones del tipo:
+                        Error: java.lang.NullPointerException: Cannot invoke "String.equalsIgnoreCase(String)" 
+                            because the return value of "java.sql.ResultSet.getString(String)" is null
+                */
+                if (rs.getString("procedencia") != null) {
+                    if (rs.getString("procedencia").equalsIgnoreCase("Spain")) {
+                        System.out.println("Nombre:" + rs.getString("Nombre") + ". Equipo: " + rs.getString("nombre_Equipo"));
+                    }
+                }
+
+            }
+
+        } catch (SQLException sqlex) {
             System.err.println("Ha habido un error a la hora de trabajar con la base de datos: " + sqlex);
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Error: " + e);
         }
     }
-    
+
 }
